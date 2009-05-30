a8a2f194ac0d9c90fa2ec51fcde1ecf4cc984e12
manuelsoal <manuelsoal@eac5e122-48f0-11de-988a-a513c5287034>
2009-05-30 16:51:27 +0000

    algunas correcciones

--- trunk/JuanecoSW/src/Snake/Canvas.java
+++ trunk/JuanecoSW/src/Snake/Canvas.java
@@ -2,8 +2,8 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package Snake;
+
 import javax.microedition.lcdui.Image;
 import javax.microedition.lcdui.Graphics;
 import javax.microedition.lcdui.game.GameCanvas;
@@ -14,133 +14,132 @@ import java.io.IOException;
  * @author Manuel Sotelo A
  */
 public class Canvas extends GameCanvas implements Runnable {
-private int tiempo_ms = 50;
-private int mov;
-Juaneco juaneco = new Juaneco();
-public final int centrox = getWidth()/2;
-public final int centroy = getHeight()/2;
-Escenario escenario = new Escenario();
 
+    private int tiempo_ms = 50;
+    private int mov;
+    Juaneco juaneco = new Juaneco();
+    public final int centrox = getWidth() / 2;
+    public final int centroy = getHeight() / 2;
+    Escenario escenario = new Escenario();
 
+    public Canvas() {
+        super(true);
 
+    }
 
- public Canvas() {
- super(true);
-
- }
-public void start() {
-try{
+    public void start() {
+        try {
 
 
 //Se cargan las imagenes de la cabeza de Juaneco por cada punto cardinal
-juaneco.setCabezaIzq(Image.createImage(Canvas.class.getResourceAsStream("/worm2.gif")));
-juaneco.setCabezaAba(Image.createImage(Canvas.class.getResourceAsStream("/worm3.gif")));
-juaneco.setCabezaDer(Image.createImage(Canvas.class.getResourceAsStream("/worm4.gif")));
-juaneco.setCabezaArr(Image.createImage(Canvas.class.getResourceAsStream("/worm1.gif")));
+            juaneco.setCabezaIzq(Image.createImage(Canvas.class.getResourceAsStream("/worm2.gif")));
+            juaneco.setCabezaAba(Image.createImage(Canvas.class.getResourceAsStream("/worm3.gif")));
+            juaneco.setCabezaDer(Image.createImage(Canvas.class.getResourceAsStream("/worm4.gif")));
+            juaneco.setCabezaArr(Image.createImage(Canvas.class.getResourceAsStream("/worm1.gif")));
 
 //Se carga la cabeza principal de juaneco como la cabezaIzq para empezar
-juaneco.setCabeza(juaneco.getCabezaIzq());
+            juaneco.setCabeza(juaneco.getCabezaIzq());
 
-juaneco.setPosicionX(centrox);
-juaneco.setPosicionY(centroy);
-escenario.setBorde(0);
+            juaneco.setPosicionX(centrox);
+            juaneco.setPosicionY(centroy);
+            escenario.setBorde(0);
 
-}
-catch(IOException e){
-System.err.println(e);
-}
-Thread hilo = new Thread (this);
+        } catch (IOException e) {
+            System.err.println(e);
+        }
+        Thread hilo = new Thread(this);
 
-hilo.start ();
-}
+        hilo.start();
+    }
 
-public void run(){
-Graphics g = getGraphics();
+    public void run() {
+        Graphics g = getGraphics();
 
-    while (true){
+        while (true) {
 
-verifyGameState();
+            verifyGameState();
 
-checkUserInput();
+            checkUserInput();
 
-updateGameScreen(getGraphics());
-        try {
-        Thread.sleep(tiempo_ms);
-        }catch (InterruptedException e ){
-        e.printStackTrace();
+            updateGameScreen(getGraphics());
+            try {
+                Thread.sleep(tiempo_ms);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
     }
-}
 
-private void checkUserInput(){
+    private void checkUserInput() {
 
-int estado_boton = getKeyStates();
+        int estado_boton = getKeyStates();
 
-cambio_coor(estado_boton);
-if (mov == 1){
-juaneco.movizq(escenario.getBorde());
-juaneco.setCabeza(juaneco.getCabezaIzq());
-}
+        cambio_coor(estado_boton);
+            }
 
-if (mov == 2){
-juaneco.movder(escenario.getBorde());
-juaneco.setCabeza(juaneco.getCabezaDer());
-}
-if (mov == 3){
+    private void movcon(){
+    if (mov == 1) {
+            juaneco.movizq(escenario.getBorde());
+            juaneco.setCabeza(juaneco.getCabezaIzq());
+        }
 
-juaneco.movup(escenario.getBorde());
-juaneco.setCabeza(juaneco.getCabezaArr());
+        if (mov == 2) {
+            juaneco.movder(escenario.getBorde());
+            juaneco.setCabeza(juaneco.getCabezaDer());
+        }
+        if (mov == 3) {
 
-
-}
-if (mov == 4){
-
-juaneco.movdwn(escenario.getBorde());
-juaneco.setCabeza(juaneco.getCabezaAba());
-
-}
-
-}
-
-private void verifyGameState(){
+            juaneco.movup(escenario.getBorde());
+            juaneco.setCabeza(juaneco.getCabezaArr());
 
 
-}
+        }
+        if (mov == 4) {
 
-private void updateGameScreen(Graphics g){
-g.setColor(0x00acde);
-g.fillRect(0, 0, getWidth(), getHeight());
+            juaneco.movdwn(escenario.getBorde());
+            juaneco.setCabeza(juaneco.getCabezaAba());
 
-g.drawImage(juaneco.getCabeza(),juaneco.getPosicionX() , juaneco.getPosicionY(), Graphics.HCENTER|Graphics.BOTTOM);
+        }
+    }
+
+    private void verifyGameState() {
+    }
+
+    private void updateGameScreen(Graphics g) {
+        movcon();
+        g.setColor(0x00acde);
+        g.fillRect(0, 0, getWidth(), getHeight());
+
+        g.drawImage(juaneco.getCabeza(), juaneco.getPosicionX(), juaneco.getPosicionY(), Graphics.HCENTER | Graphics.BOTTOM);
 
 
-flushGraphics();
+        flushGraphics();
 
-}
+    }
 
-private void cambio_coor (int estado_boton){
+    private void cambio_coor(int estado_boton) {
 
-if (((estado_boton &LEFT_PRESSED) != 0)&&(mov !=1)){
-if (mov != 2) {
-    juaneco.movizq(escenario.getBorde());
-    mov = 1;
-}
-} else if (((estado_boton &RIGHT_PRESSED) != 0  )&&(mov !=2)){
-if (mov != 1) {
-    juaneco.movder(escenario.getBorde());
-    mov = 2;
-}
-} else if (((estado_boton &UP_PRESSED) != 0)&&(mov !=3)){
-if (mov != 4) {
-    juaneco.movup(escenario.getBorde());
-    mov = 3;
-}
-} else if (((estado_boton &DOWN_PRESSED) != 0)&&(mov !=4)){
-if (mov != 3) {
-    juaneco.movdwn(escenario.getBorde());
-    mov = 4;
-}
-}
+        if (((estado_boton & LEFT_PRESSED) != 0) && (mov != 1)) {
+            if (mov != 2) {
+                juaneco.movizq(escenario.getBorde());
+                mov = 1;
+            }
+        } else if (((estado_boton & RIGHT_PRESSED) != 0) && (mov != 2)) {
+            if (mov != 1) {
+                juaneco.movder(escenario.getBorde());
+                mov = 2;
+            }
+        } else if (((estado_boton & UP_PRESSED) != 0) && (mov != 3)) {
+            if (mov != 4) {
+                juaneco.movup(escenario.getBorde());
+                mov = 3;
+            }
+        } else if (((estado_boton & DOWN_PRESSED) != 0) && (mov != 4)) {
+            if (mov != 3) {
+                juaneco.movdwn(escenario.getBorde());
+                mov = 4;
+            }
+        }
 
-}
+    }
 }
