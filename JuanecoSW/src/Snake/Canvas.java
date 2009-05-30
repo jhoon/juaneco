a76796d983773575c55381f4048d786e151501df
manuelsoal <manuelsoal@eac5e122-48f0-11de-988a-a513c5287034>
2009-05-30 18:09:24 +0000

    algunas correcciones

--- JuanecoSW/src/Snake/Canvas.java
+++ JuanecoSW/src/Snake/Canvas.java
@@ -8,20 +8,22 @@ import javax.microedition.lcdui.Image;
 import javax.microedition.lcdui.Graphics;
 import javax.microedition.lcdui.game.GameCanvas;
 import java.io.IOException;
-
+import java.util.Random;
 /**
  *
  * @author Manuel Sotelo A
  */
 public class Canvas extends GameCanvas implements Runnable {
-
+    Random rndx = new Random();
+    Random rndy = new Random();
     private int tiempo_ms = 50;
     private int mov;
     Juaneco juaneco = new Juaneco();
     public final int centrox = getWidth() / 2;
     public final int centroy = getHeight() / 2;
     Escenario escenario = new Escenario();
-
+    Animales animal = new Animales ();
+    private int choque = 0;
     public Canvas() {
         super(true);
 
@@ -36,12 +38,13 @@ public class Canvas extends GameCanvas implements Runnable {
             juaneco.setCabezaAba(Image.createImage(Canvas.class.getResourceAsStream("/worm3.gif")));
             juaneco.setCabezaDer(Image.createImage(Canvas.class.getResourceAsStream("/worm4.gif")));
             juaneco.setCabezaArr(Image.createImage(Canvas.class.getResourceAsStream("/worm1.gif")));
-
+            animal.setFigura(Image.createImage(Canvas.class.getResourceAsStream("/link.gif")));
 //Se carga la cabeza principal de juaneco como la cabezaIzq para empezar
-            juaneco.setCabeza(juaneco.getCabezaIzq());
-
+            juaneco.setCabeza(juaneco.getCabezaArr());
             juaneco.setPosicionX(centrox);
             juaneco.setPosicionY(centroy);
+            animal.setPosicionX(rndx.nextInt(getWidth()));
+            animal.setPosicionY(rndy.nextInt(getHeight()));
             escenario.setBorde(0);
 
         } catch (IOException e) {
@@ -101,18 +104,25 @@ public class Canvas extends GameCanvas implements Runnable {
 
         }
     }
+private void shock (){
 
+if((juaneco.getPosicionX() == animal.getPosicionX())&(juaneco.getPosicionY() == animal.getPosicionY()))
+ choque = 1;
+
+}
     private void verifyGameState() {
     }
 
     private void updateGameScreen(Graphics g) {
         movcon();
+        shock();
         g.setColor(0x00acde);
         g.fillRect(0, 0, getWidth(), getHeight());
 
         g.drawImage(juaneco.getCabeza(), juaneco.getPosicionX(), juaneco.getPosicionY(), Graphics.HCENTER | Graphics.BOTTOM);
-
-
+        if(choque == 0)
+        g.drawImage(animal.getFigura(), animal.getPosicionX(), animal.getPosicionY(), Graphics.HCENTER | Graphics.BOTTOM);
+        
         flushGraphics();
 
     }
