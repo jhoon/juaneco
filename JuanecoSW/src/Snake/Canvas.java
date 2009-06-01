f4e8599ba1877680eaae5afd610dcfec6c6c8e04
manuelsoal <manuelsoal@eac5e122-48f0-11de-988a-a513c5287034>
2009-06-01 15:33:07 +0000

    Solucion x xD

--- JuanecoSW/src/Snake/Canvas.java
+++ JuanecoSW/src/Snake/Canvas.java
@@ -30,10 +30,8 @@ public class Canvas extends GameCanvas implements Runnable {
     private final int tiempo_ms = 50;
 // Variables del juego
     private int mov;
-    private int choque = 0;
-    private int[][] posicion = new int[lado / 16][lado / 16];
-    int arx ;//Lo del arreglo pra X
-    int ary ;//Lo del arrego para Y
+    private int[][] posesc = new int[lado / 16][lado / 16];
+    private int[][] poscel = new int[lado / 16][lado / 16];
 
     public Canvas() {
         super(true);
@@ -50,25 +48,22 @@ public class Canvas extends GameCanvas implements Runnable {
             escenario.setFondo(Image.createImage(Canvas.class.getResourceAsStream("/fondo1.jpg")));
 // Se inicializa a juaneco, escenario y comida
             juaneco.setIncremento(0);
-            juaneco.setPosicionX(lado/2);
-            juaneco.setPosicionY(lado/2);
-            animal.setPosicionX(rndx.nextInt(getWidth()));
-            animal.setPosicionY(rndy.nextInt(getHeight()));
             escenario.setBorde(1);
             juaneco.setVelocidad(5);
-            arx =  lado/32 -1;
-            ary =  lado/32 -1;
+
 // Se crea la matriz de movimiento
             for (int i = 0; i < lado / 16; i++) {
                 for (int j = 0; j < lado / 16; j++) {
-                    if ((i == (arx)) & (j == (ary - 1))) {
-                        posicion[i][j] = 1;
-                    } else {
-                        posicion[i][j] = 0;
-                    }
+                    poscel[i][j] = i * j * 8;
                 }
             }
-
+// Se crea la matriz de posiciones de objetos (inicializa)            
+            for (int i = 0; i < lado / 16; i++) {
+                for (int j = 0; j < lado / 16; j++) {
+                    posesc[i][j] = 0;
+                }
+            }
+// Se cargan las primeras imagenes de juaneco
 
         } catch (IOException e) {
             System.err.println(e);
@@ -104,42 +99,29 @@ public class Canvas extends GameCanvas implements Runnable {
     }
 
     private void movcon() {
-        arx = juaneco.getPosicionX()/16;
-        ary = juaneco.getPosicionY()/16;
-        if (posicion[arx-1][ary]==0) {
+        if (mov == 1) {
             juaneco.movizq(escenario.getBorde(), lado);
             juaneco.setCabeza(juaneco.getCabezaIzq());
-            //posicion[arx/20][ary/20] = 1;
-
         }
-
-        if (posicion[arx+1][ary]==0) {
+        if (mov == 2) {
             juaneco.movder(escenario.getBorde(), lado);
-            juaneco.setCabeza(juaneco.getCabezaDer());
+            juaneco.setCabeza(juaneco.getCabezaIzq());
         }
-        if (posicion[arx][ary-1]==0) {
-
+        if (mov == 3) {
             juaneco.movup(escenario.getBorde(), lado);
-            juaneco.setCabeza(juaneco.getCabezaArr());
+            juaneco.setCabeza(juaneco.getCabezaIzq());
         }
-        if (posicion[arx][ary+1]==0) {
-
+        if (mov == 4) {
             juaneco.movdwn(escenario.getBorde(), lado);
-            juaneco.setCabeza(juaneco.getCabezaAba());
-
+            juaneco.setCabeza(juaneco.getCabezaIzq());
         }
+
     }
 
     private void shock() {
-
-        if ((juaneco.getPosicionX() == animal.getPosicionX()) & (juaneco.getPosicionY() == animal.getPosicionY())) {
-            choque = 1;
-        }
-
     }
 
     private void verifyGameState() {
-        
     }
 
     private void updateGameScreen(Graphics g) {
@@ -150,36 +132,23 @@ public class Canvas extends GameCanvas implements Runnable {
         g.fillRect(0, 0, getWidth(), getHeight());
         g.drawImage(escenario.getFondo(), centrox, centroy * 2, Graphics.HCENTER | Graphics.BOTTOM);
         g.drawImage(juaneco.getCabeza(), juaneco.getPosicionX(), juaneco.getPosicionY(), Graphics.HCENTER | Graphics.BOTTOM);
-        if (choque == 0) {
-            g.drawImage(animal.getFigura(), animal.getPosicionX(), animal.getPosicionY(), Graphics.HCENTER | Graphics.BOTTOM);
-        }
 
-
-        g.drawString("posicion1posicion2 "+posicion[arx][ary], centrox, centroy - juaneco.getVelocidad(), Graphics.BASELINE | Graphics.HCENTER);
-        flushGraphics();
 
     }
 
     private void cambio_coor(int estado_boton) {
-        if (((estado_boton & LEFT_PRESSED) != 0)&(posicion[arx - 1][ary] == 0)) {
+        if (((estado_boton & LEFT_PRESSED) != 0)) {
             juaneco.movizq(escenario.getBorde(), lado);
-            posicion[arx][ary] = 1;
-            posicion[arx+1][ary]=0;
-        } else if (((estado_boton & RIGHT_PRESSED) != 0)&(posicion[arx + 1][ary] == 0)) {
-
+            mov = 1;
+        } else if (((estado_boton & RIGHT_PRESSED) != 0)) {
             juaneco.movder(escenario.getBorde(), lado);
-            posicion[arx][ary] = 1;
-            posicion[arx-1][ary]=0;
-        } else if (((estado_boton & UP_PRESSED) != 0)&(posicion[arx][ary - 1] == 0)) {
-
+            mov = 2;
+        } else if (((estado_boton & UP_PRESSED) != 0)) {
             juaneco.movup(escenario.getBorde(), lado);
-            posicion[arx][ary] = 1;
-            posicion[arx][ary+1]=0;
-        } else if (((estado_boton & DOWN_PRESSED) != 0)&(posicion[arx][ary + 1] == 0)) {
-
+            mov = 3;
+        } else if (((estado_boton & DOWN_PRESSED) != 0)) {
             juaneco.movdwn(escenario.getBorde(), lado);
-            posicion[arx][ary] = 1;
-            posicion[arx][ary-1]=0;
+            mov = 4;
         }
 
     }
